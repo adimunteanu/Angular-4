df0e3e67ad4aabbad10c4a3786e35428aba32cd5
unknown <adimunteanu13@gmail.com>
2017-05-24 22:49:24 +0300

    More routing

--- cmp-databinding-start/src/app/cockpit/cockpit.component.ts
+++ cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,EventEmitter,Output } from '@angular/core';
+import { Component, OnInit,EventEmitter,Output,ViewChild,ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -9,22 +9,24 @@ export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter<{serverName:string,serverContent:string}>();
   @Output('bpCreated') blueprintCreated= new EventEmitter<{blueprintName:string,blueprintContent:string}>();
   //newServerName = '';
-  newServerContent = '';
+  // newServerContent = '';
+  @ViewChild('serverContentInput') serverContentInput: ElementRef;
   constructor() { }
 
   ngOnInit() {
   }
   onAddServer(nameInput: HTMLInputElement) {
+    console.log(this.serverContentInput);
     this.serverCreated.emit({
       serverName: nameInput.value,
-      serverContent:this.newServerContent
+      serverContent:this.serverContentInput.nativeElement.value
     });
   }
 
   onAddBlueprint(nameInput: HTMLInputElement) {
     this.blueprintCreated.emit({
       blueprintName: nameInput.value,
-      blueprintContent:this.newServerContent
+      blueprintContent:this.serverContentInput.nativeElement.value
     });
   }
 }
