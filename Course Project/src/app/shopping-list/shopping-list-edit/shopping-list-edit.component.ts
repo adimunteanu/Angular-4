28567b8b6de055090c203ccc22e1ab0997a764bc
unknown <adimunteanu13@gmail.com>
2017-06-27 14:58:25 +0300

    Course Project TD Forms

--- Course Project/src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ Course Project/src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,23 +1,60 @@
+import { Subscription } from 'rxjs/Subscription';
 import { ShoppingListService } from './../shopping-list.service';
-import { Component, OnInit,ElementRef,ViewChild } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
 import {Ingredient} from '../../shared/ingredient.model';
+import { NgForm } from "@angular/forms";
 @Component({
   selector: 'app-shopping-list-edit',
   templateUrl: './shopping-list-edit.component.html',
   styleUrls: ['./shopping-list-edit.component.css']
 })
-export class ShoppingListEditComponent implements OnInit {
-  @ViewChild('nameInput') nameInputRef: ElementRef;
-  @ViewChild('amountInput') amountInputRef: ElementRef;
- 
-  onAddItem(){
-    const ingName=this.nameInputRef.nativeElement.value;
-    const ingAmount=this.amountInputRef.nativeElement.value;
-    const newIngredient=new Ingredient(ingName,ingAmount);
-    this.shoppingListService.addIngredient(newIngredient);
-  }
-  constructor(private shoppingListService:ShoppingListService) { }
+export class ShoppingListEditComponent implements OnInit,OnDestroy {
+  @ViewChild('f') slForm: NgForm;
+  subscription: Subscription;
+  editMode= false;
+  editedItemIndex: number;
+  editedItem: Ingredient;
+
+  constructor(private slService: ShoppingListService) { }
+
   ngOnInit() {
+    this.subscription = this.slService.startedEditing.subscribe(
+      (index: number) => {
+        this.editedItemIndex = index;
+        this.editMode = true;
+        this.editedItem = this.slService.getIngredient(index);
+        this.slForm.setValue({
+          name: this.editedItem.name,
+          amount: this.editedItem.amount
+        });
+      }
+    );
   }
 
+  onSubmit(form: NgForm) {
+    const value = form.value;
+    console.log(value);
+    const newIngredient = new Ingredient(value.name, value.amount);
+    if(this.editMode){
+      this.slService.updateIngredient(this.editedItemIndex, newIngredient);
+    } else {
+      this.slService.addIngredient(newIngredient);
+    }
+    this.editMode = false;
+    form.reset();
+  }
+
+  onClear(){
+    this.slForm.reset();
+    this.editMode = false;
+  }
+  
+  onDelete(){
+    this.slService.deleteIngredient(this.editedItemIndex);
+    this.onClear();
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
+  }
 }
--- Course Project/src/app/shopping-list/shopping-list.service.ts
+++ Course Project/src/app/shopping-list/shopping-list.service.ts
@@ -3,16 +3,33 @@ import { EventEmitter } from '@angular/core';
 import { Ingredient } from './../shared/ingredient.model';
 import { Subject } from "rxjs/Subject";
 export class ShoppingListService {
-    ingredientsChanged=new Subject<Ingredient[]>();
-    private ingredients: Ingredient[]=[
-        new Ingredient('Apples',5),
-        new Ingredient('Tomatoes',10)
+    ingredientsChanged= new Subject<Ingredient[]>();
+    startedEditing= new Subject<number>();
+
+    private ingredients: Ingredient[]= [
+        new Ingredient('Apples', 5),
+        new Ingredient('Tomatoes', 10)
     ];
     getIngredients(){
         return this.ingredients.slice();
     }
-    addIngredient(ingredient:Ingredient){
+    addIngredient(ingredient: Ingredient) {
         this.ingredients.push(ingredient);
+        this.ingredientsChanged.next(this.ingredients.slice());
+    }
+
+    getIngredient(index: number) {
+        return this.ingredients[index];
+    }
+    
+    updateIngredient(index: number, newIngredient: Ingredient){
+        this.ingredients[index] = newIngredient;
+        this.ingredientsChanged.next(this.ingredients.slice());
+    }
+
+    deleteIngredient(index: number){
+        console.log('delete works');
+        this.ingredients.splice(index, 1);
         this.ingredientsChanged.next(this.ingredients.slice());
     }
 }
